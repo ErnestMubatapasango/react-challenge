17de5bcda5bf9e21c1ccbf83ed77d85fac8adf22
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-05-30 17:31:50 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+  const numbers = [0,1,2,3,4,5,6,7,8,9]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-row flex-wrap justify-center items-center gap-5 mt-[200px] mx-[600px] border-green-200 border-2 p-5'>
+      <input className='border-2 border-green-200 p-1 rounded-full ' type='number' placeholder='enter the passcode'/>
+      {numbers.map( number => {
+        return (
+          <div>
+             <button className='px-4 rounded-full bg-green-200 cursor-pointer text-lg font-medium'>{number}</button>
+          </div>
+           
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
